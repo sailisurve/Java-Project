40e1c6675ab7e5909de3fabc1141a24d3ac7c674
sailisurve <165483521+sailisurve@users.noreply.github.com>
2024-08-30 18:45:23 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -37,7 +37,7 @@ public class Client {
     
     public static void main(String[] args){
         Client client = new Client();
-        for(int i=0; i<25; i++){
+        for(int i=0; i<100; i++){
             try{
                 Thread thread = new Thread(client.getRunnable());
                 thread.start();
